4ccdafd718380c4ab3f765aebbe9959b1e04955a
user <user@DESKTOP-DD1FIU1>
2023-06-30 09:46:44 +0300

    Add project files.

---
+++ eCommerceTest/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace eCommerceTest.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        //private readonly ApplicationDbContext _db;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ eCommerceTest/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceTest.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,99,ErrorMessage = "Display Order must be between 1 and 99!!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+= DateTime.Now;    
+    }
+}
